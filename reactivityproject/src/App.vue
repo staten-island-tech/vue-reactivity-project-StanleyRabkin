09538fe6834de703f4a6fc8c79a208a500aa6fde
StanleyRabkin <71650302+StanleyRabkin@users.noreply.github.com>
2021-10-18 11:49:09 -0400

    qwikjyht

--- reactivityproject/src/App.vue
+++ reactivityproject/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Hi"/>
+    <HelloWorld msg="I Lke Oreos"/>
   </div>
 </template>
 
@@ -18,11 +18,5 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
